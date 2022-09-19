e8c6518e9acc0254b79b20828ac318ecb5f99457
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-09-19 19:28:08 -0400

    Main Addition is time I wrote others in readme

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Day-Scheduler
 
 ## Description
-
+- I began by adjusting the HTML by adding a regular link to css and a script to js.
+- I added a starter variable to hold the foundation of my js.
+- I created a array for loop for the time presented.
+- I added variables to show the time as well as hold data.
+- 
 ### Screenshot
 
 #### URL
---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// Start Variable
+var workdaySchedule = []
+
+// This loop will create a array FOR loop
+for (time = 5; time <=20; time++) {
+    var id = time - 5
+    var scheduleData = ""
+
+// This will show the time
+var showHour = 0;
+var dayTimer = "";
+
+if (time === 12) {
+    showHour = 12
+    dayTimer = "pm"
+} else if (time > 12) {
+    showHour = time - 12;
+    dayTimer = "pm";
+} else if (time < 12) {
+    showHour = time;
+    dayTimer = "am";
+}
+
+showHour = showHour.toString()
+
+// Holds information within this section
+scheduleData = {
+    id: id,
+    time: time,
+    scheduleData: scheduleData,
+    showHour: showHour,
+    dayTimer: dayTimer
+}
+
+workdaySchedule.push(scheduleData)
+
+}
